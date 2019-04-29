5f74fffff1590ee41ce339eeeb686d52795092e1
Andrew Sievers <andrew.sievers@selu.edu>
2019-04-29 18:51:32 -0500

    trying

--- server.js
+++ server.js
@@ -43,6 +43,14 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:2701
   });
 });
 
+// CONTACTS API ROUTES BELOW
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+    console.log("ERROR: " + reason);
+    res.status(code || 500).json({"error": message});
+  }
+
 // GET https://polar-castle-32257.herokuapp.com/rest/list
 // returns list of tickets in memoryd
 router.get('/list', function(req, res) {
